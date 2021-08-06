ca349958a642f82b76c278f09f1c8d0d12889d8f
Tatiana <poluninat.m@gmail.com>
2021-08-06 11:39:39 +0300

    add index.php

---
+++ http/index.php
@@ -0,0 +1,45 @@
+<?php
+
+use GuzzleHttp\Psr7\ServerRequest;
+use totum\common\controllers\Controller;
+use totum\config\Conf;
+
+$GLOBALS['mktimeStart'] = microtime(true);
+
+ini_set('log_errors', 1);
+ini_set('display_errors', 1);
+ini_set('error_reporting', E_ALL);
+ignore_user_abort(false);
+
+require __DIR__ . '/../../../autoload.php';
+
+if (!class_exists(Conf::class)) {
+    $Config = null;
+    list($module, $lastPath) = ['install', ''];
+} else {
+    $Config = new Conf();
+    if (is_callable([$Config, 'setHostSchema'])) {
+        $Config->setHostSchema($_SERVER['HTTP_HOST']);
+    }
+    list($module, $lastPath) = $Config->getActivationData($_SERVER['REQUEST_URI']);
+}
+
+if (empty($module)) {
+    $module = 'Table';
+    $lastPath = '';
+}
+$controllerClass = 'totum\\moduls\\' . $module . '\\' . $module . 'Controller';
+if (class_exists($controllerClass)) {
+    /*
+     * @var Controller $Controller
+     * */
+    $Controller = new $controllerClass($Config);
+
+    $request = ServerRequest::fromGlobals();
+
+    $response = $Controller->doIt($request, true);
+    //$Config->getSql()->transactionRollBack();
+    die;
+} else die('Не найдено ' . $controllerClass);
+
+?>
