65621e21284ac1581368c7a76109a13b6c8001b9
Tatiana <poluninat.m@gmail.com>
2021-11-11 16:39:17 +0300

    кнопка откл админского инт показывается при перекл в прост пользователя

--- modules/App/reUserInterface.js
+++ modules/App/reUserInterface.js
@@ -11,6 +11,29 @@
 
         if (isNotCreatorHimSelf) {
             App.blink(UserFio, 10, '#ffe486');
+        }else if(App.isTopWindow()){
+
+            if ($('#isCreator').length === 0) {
+                let isMobile = screen.width <= window.MOBILE_MAX_WIDTH;
+                let checkbox = $('<span id="isCreator" class="btn btn-sm"><i class="fa-user-circle fa"></i></span>');
+                let input = checkbox;
+                if (!isMobile && !localStorage.getItem('notCreator')) {
+                    input.addClass('btn-danger');
+                } else {
+                    input.addClass('btn-warning');
+                    $('.plus-top-branch').hide();
+                }
+                input.on('click', () => {
+                    if (!localStorage.getItem('notCreator')) {
+                        localStorage.setItem('notCreator', true)
+                    } else {
+                        localStorage.removeItem('notCreator')
+                    }
+                    window.location.reload(true);
+                })
+                $('#docs-link').before(checkbox)
+            }
+
         }
 
         const addFioClick = function (event) {
