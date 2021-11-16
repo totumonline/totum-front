39ee67ce90e81742fb525b04b1154346b389bdcf
Tatiana <poluninat.m@gmail.com>
2021-11-16 14:14:37 +0300

    adding function panelButton

--- modules/Bootstrap.Helpers/Datepicker.defaults.js
+++ modules/Bootstrap.Helpers/Datepicker.defaults.js
@@ -1,5 +1,4 @@
 (function () {
-
     $.fn.datetimepicker.defaults.icons.close = 'glyphicon glyphicon-ok';
     setTimeout(()=>{
         $.fn.datetimepicker.defaults.tooltips.close = App.translate('Apply and close');
