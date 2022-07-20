36d0ebdd678d12ec0d8064b8cb0f30f0a7142c65
Tatiana <poluninat.m@gmail.com>
2022-07-20 18:09:21 +0300

    forms localisation

---
+++ Forms/js/components/lang/ru.js
@@ -0,0 +1,21 @@
+export const ruLang = {
+    'Cancel': 'Отмена',
+    'Confirmation': 'Подтверждение',
+    'Surely to change?': 'Точно изменить?',
+    'Done': 'Выполнено',
+    'Mandatory Field': 'Обязательное поле',
+    'Clear': 'Очистить',
+    'Upload files': 'Загрузить файлы',
+    'Files uploading': 'Загрузка файлов',
+    'Drag and drop files to download': 'Перетащите файлы для загрузки',
+    'Confirm': 'Подтвердить',
+    'OK': 'ОК',
+    'Error': 'Ошибка',
+    'Data loading': 'Загрузка данных',
+    'The data is incomplete, use the search': 'Данные не полны, воспользуйтесь поиском',
+    'Selected': 'Выбранное',
+    'Nothing found': 'Ничего не найдено',
+    'Regexp does not pass':'Regexp не проходит',
+    "The value should not be empty":"Значение не должно быть пустым"
+
+}
