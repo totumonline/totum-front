b72ed3ce5601f7217af3de515cc8693000232c2e
Tatiana <poluninat.m@gmail.com>
2022-12-26 18:55:46 +0300

    fx forms

--- Forms/js/App.jsx
+++ Forms/js/App.jsx
@@ -9,6 +9,7 @@ import {ruLang} from './components/lang/ru.js'
 
 const theme = createMuiTheme({
     typography: {
+        fontSize: 16,
         fontFamily: [
             'IBM Plex Sans',
             'Helvetica',
