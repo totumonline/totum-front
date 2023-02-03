060f6912999269f904b68b46371c243cc13f4009
Tatiana <poluninat.m@gmail.com>
2023-02-03 13:42:35 +0300

    change mobile width

--- modules/table/table.js
+++ modules/table/table.js
@@ -3,7 +3,7 @@
     let BUTTONS_TIMEOUT = 2000;
 
     const pcTABLE_ROW_HEIGHT = 35;
-    window.MOBILE_MAX_WIDTH = 1199;
+    window.MOBILE_MAX_WIDTH = 1030;
 
     const pcTable_DATA_KEY = 'pctable';
     const pcTable_DATA_INDEX = 'pctablettemtndex';
