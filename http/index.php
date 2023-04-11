6131c874c9d52719b63fd10d65ecb26841cfd9da
Tatiana <poluninat.m@gmail.com>
2023-04-11 17:26:47 +0300

    hidden host lang

--- http/index.php
+++ http/index.php
@@ -1,14 +1,10 @@
 <?php
 
 use GuzzleHttp\Psr7\ServerRequest;
-use totum\common\controllers\Controller;
 use totum\config\Conf;
 
 $GLOBALS['mktimeStart'] = microtime(true);
 
-ini_set('log_errors', 1);
-ini_set('display_errors', 1);
-ini_set('error_reporting', E_ALL);
 ignore_user_abort(false);
 
 require __DIR__ . '/../../../autoload.php';
@@ -22,12 +18,13 @@ if (!class_exists(Conf::class)) {
         $Config->setHostSchema($_SERVER['HTTP_HOST']);
     }
 
-    if ($_SERVER['REQUEST_URI'] === '/ServicesAnswer' || str_starts_with($_SERVER['REQUEST_URI'], '/ServicesAnswer?')) {
+    if (preg_match('/^\/ServicesAnswer(\/|\?|$)/', $_SERVER['REQUEST_URI'])) {
         \totum\common\Services\ServicesConnector::init($Config)->setAnswer(ServerRequest::fromGlobals());
         die('true');
     }
     list($module, $lastPath) = $Config->getActivationData($_SERVER['REQUEST_URI']);
 }
+
 
 if (empty($module)) {
     $module = 'Table';
@@ -35,9 +32,14 @@ if (empty($module)) {
 }
 $controllerClass = 'totum\\moduls\\' . $module . '\\' . $module . 'Controller';
 if (class_exists($controllerClass)) {
-
-    if($Config && !empty($Config->getHiddenHosts()[$Config->getFullHostName()]) && empty($Config->getHiddenHosts()[$Config->getFullHostName()][$module])){
-        die($Config->getLangObj()->translate('The module is not available for this host.'));
+    if ($Config && !empty($Config->getHiddenHosts()[$Config->getFullHostName()])) {
+        if(empty($Config->getHiddenHosts()[$Config->getFullHostName()][$module])){
+            die($Config->getLangObj()->translate('The module is not available for this host.'));
+        }else{
+            if(is_array($Config->getHiddenHosts()[$Config->getFullHostName()][$module])){
+                $Config->setHiddenHostSettings($Config->getHiddenHosts()[$Config->getFullHostName()][$module]);
+            }
+        }
     }
 
     /*
@@ -46,10 +48,17 @@ if (class_exists($controllerClass)) {
     $Controller = new $controllerClass($Config);
 
     $request = ServerRequest::fromGlobals();
-
     $response = $Controller->doIt($request, true);
-    //$Config->getSql()->transactionRollBack();
-    die;
-} else die('Not found: ' . $controllerClass);
 
-?>
+
+    //$Config->getSql()->transactionRollBack();
+
+} else {
+    if ($Config) {
+        $Lang = $Config->getLangObj();
+    } else {
+        $Lang = (new \totum\common\Lang\EN());
+    }
+    echo $Lang->translate('Not found: %s', [htmlspecialchars($controllerClass)]);
+}
+die;
